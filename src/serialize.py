a6eb53a9cde4283bf91c4246cf07b0b47cacae8a
tulseyy <sashankartulsha@gmail.com>
2025-10-29 14:01:59 +0900

    week 7 assignment 4

--- src/serialize.py
+++ src/serialize.py
@@ -6,8 +6,42 @@ class Node:
 
 def serialize(root):
     # TODO: preorder with '#' for None, space-separated
-    pass
+    # preorder traversal: node, left, right
+    vals = []
+
+    def helper(node):
+        if node is None:
+            vals.append('#')
+            return
+        vals.append(str(node.val))
+        helper(node.left)
+        helper(node.right)
+
+    helper(root)
+    return ' '.join(vals)
 
 def deserialize(s):
     # TODO: rebuild from tokens
-    pass
+    if not s:
+        return None
+
+    tokens = iter(s.split())
+
+    def helper():
+        try:
+            tok = next(tokens)
+        except StopIteration:
+            return None
+        if tok == '#':
+            return None
+        # try to convert to int when possible, otherwise keep string
+        try:
+            val = int(tok)
+        except ValueError:
+            val = tok
+        node = Node(val)
+        node.left = helper()
+        node.right = helper()
+        return node
+
+    return helper()
